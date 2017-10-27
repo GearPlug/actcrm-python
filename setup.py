7c59834b7595dcd667af6b8443f9d9cfc403d5f3
Gustavo Saavedra <tavito.286@gmail.com>
2017-10-27 16:12:54 -0500

    setup.py to instal via pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='typeform',
+      version='0.1',
+      description='API wrapper for Act! CRM written in Python',
+      url='https://github.com/GearPlug/actcrm-python',
+      author='Gustavo Saavedra',
+      author_email='tavito.286@gmail.com',
+      license='GPL',
+      packages=['actcrm', ],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
