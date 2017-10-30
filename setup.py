81d68328359732a1e8ae43257cfda2af70084a6f
Gustavo Saavedra <tavito.286@gmail.com>
2017-10-30 10:57:28 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='typeform',
+setup(name='actcrm-python',
       version='0.1',
       description='API wrapper for Act! CRM written in Python',
       url='https://github.com/GearPlug/actcrm-python',
